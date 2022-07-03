26f87873a52aa9a8cd7affdf1a66fd21e9b34e20
emyslandim <emydl.moura@gmail.com>
2022-07-03 11:35:03 -0300

    fix link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook, faCalendarAlt, faComment, faHome, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import './styles.css';
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 function Footer() {
 
     return (
@@ -18,14 +17,14 @@ function Footer() {
             <div className="footer-link-home">
                 <FontAwesomeIcon icon={faHome} color="#fff" />
             </div>
-            <div className="footer-link">
+            <Link to="/chat" className="footer-link">
                 <FontAwesomeIcon icon={faComment} color="#626162" />
                 <span className="footer-link-text">chama a kira</span>
-            </div>
-            <Link to="/chat" className="footer-link">
+            </Link>
+            <div className="footer-link">
                 <FontAwesomeIcon icon={faShoppingCart} color="#626162" />
                 <span className="footer-link-text">shopping</span>
-            </Link>
+            </div>
         </div>
     );
 }

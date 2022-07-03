26a7284132264a8ab63670e6d063e5a315cc9608
emyslandim <emydl.moura@gmail.com>
2022-07-02 21:38:53 -0300

    install kotex fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home';
+import './fonts/Kotex-Bold.ttf'
+import './fonts/Kotex-Light.ttf'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

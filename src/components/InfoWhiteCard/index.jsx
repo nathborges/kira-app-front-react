8b1695f24d7be3a4d7bfbc22a7b9ceb1ae7476de
emyslandim <emydl.moura@gmail.com>
2022-07-02 21:40:00 -0300

    Create infowhitecard component

---
+++ src/components/InfoWhiteCard/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import './styles.css';
+
+function InfoWhiteCard() {
+  return (
+    <div className="info-white-card">
+      <FontAwesomeIcon className="info-white-card-icon" icon={faBook} color='#f62459' size={10} />
+      <span className="info-white-card-text">personalize o app para que possamos trazer conteúdos especialmente para você.</span>
+    </div>
+  );
+}
+
+export default InfoWhiteCard;
